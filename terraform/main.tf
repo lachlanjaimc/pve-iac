e75664bcbecc7966b7c1fe3f990f7d27c4272f7d
lachlanjaimc <lachlanjaimc@gmail.com>
2026-06-22 14:22:50 -0600

    Adding an example resource - new PVE user

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,19 @@ provider "proxmox" {
   password = ${{ secrets.PROXMOX_PASSWORD }}
   insecure = true
 }
+
+# if creating a user token, the user must be created first
+resource "proxmox_virtual_environment_user" "user" {
+  comment         = "Managed by Terraform"
+  email           = "lachlan@pve"
+  enabled         = true
+  expiration_date = "2034-01-01T22:00:00Z"
+  user_id         = "lachlan@pve"
+}
+
+resource "proxmox_user_token" "user_token" {
+  comment         = "Managed by Terraform"
+  expiration_date = "2033-01-01T22:00:00Z"
+  token_name      = "tk1"
+  user_id         = proxmox_virtual_environment_user.user.user_id
+}
